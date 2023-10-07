0dd11d116a51e9a0e95aae161f89f77853f7f2c3
A B Veekshith <87522608+ABveekshith@users.noreply.github.com>
2023-10-07 22:06:18 +0530

    Create 3sum.py

---
+++ 3sum.py
@@ -0,0 +1,30 @@
+def threeSum(nums):
+    # Sort the input array
+    nums.sort()
+    result = []
+
+    for i in range(len(nums) - 2):
+        # Skip duplicates to avoid duplicate triplets
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left, right = i + 1, len(nums) - 1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+
+            if total == 0:
+                result.append([nums[i], nums[left], nums[right]])
+                # Skip duplicates
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+            elif total < 0:
+                left += 1
+            else:
+                right -= 1
+
+    return result
