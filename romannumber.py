38df967e204120c022cdea9d95792f898e7225ca
dakshrajput100 <115660640+dakshrajput100@users.noreply.github.com>
2023-10-25 10:44:00 +0530

    Create romannumber.py
    Convert number into roman number upto 1000 number

---
+++ romannumber.py
@@ -0,0 +1,79 @@
+n = int(input())
+while(True):
+    if 1000 <= n:
+        print("M",end='')
+        n = n-1000
+    else:
+        break
+while(True):    
+    if 900 <= n :
+        print("XM",end='')
+        n = n-900  
+    else:
+        break    
+while(True):    
+    if 500 <= n :
+        print("D",end='')
+        n = n-500  
+    else:
+        break
+while(True):    
+    if 400 <= n :
+        print("XD",end='')
+        n = n-400  
+    else:
+        break    
+while(True):    
+    if 100 <= n :
+        print("C",end='')
+        n = n-100  
+    else:
+        break
+while(True):    
+    if 90 <= n :
+        print("XC",end='')
+        n = n-90  
+    else:
+        break    
+while(True):    
+    if 50 <= n :
+        print("L",end='')
+        n = n-50  
+    else:
+        break
+while(True):    
+    if 40 <= n :
+        print("XL",end='')
+        n = n-40  
+    else:
+        break    
+while(True):    
+    if 10 <= n :
+        print("X",end='')
+        n = n-10  
+    else:
+        break
+while(True):    
+    if 9 <= n :
+        print("IX",end='')
+        n = n-9  
+    else:
+        break                
+while(True):    
+    if 5 <= n :
+        print("v",end='')
+        n = n-5  
+    else:
+        break
+while(True):    
+    if 4 <= n :
+        print("IV",end='')
+        n = n-4  
+    else:
+        break
+while(True):    
+    if 1 <= n :
+        print("I",end='')
+        n = n-1  
+    else:
+        break        
